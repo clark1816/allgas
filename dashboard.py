d40f900403883d68309d3ad7b911efd5477f4080
clark1816 <33646776+clark1816@users.noreply.github.com>
2021-11-11 13:13:23 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -19,7 +19,8 @@ auth = tweepy.OAuthHandler(config.TWITTER_CONSUMER_KEY, config.TWITTER_CONSUMER_
 auth.set_access_token(config.TWITTER_ACCESS_TOKEN, config.TWITTER_ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-connection = psycopg2.connect(host = 'ec2-52-4-100-65.compute-1.amazonaws.com', database='etfdb', user='postgres', password='password', port='5432')
+connection = psycopg2.connect(host = 'ec2-52-4-100-65.compute-1.amazonaws.com', database='d6sbdsmqqmsbc8', user='wtyttcbfarkbwm', password='
+9a6db6fc2338ed6456bfbe07cf31dc47b38ab2e9fba9a63c05d9e17d1e799267', port='5432')
 cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 option = st.sidebar.selectbox("Which Dashboard?", ('twitter', 'wallstreetbets', 'stocktwits', 'patterns','company info'), 1)
